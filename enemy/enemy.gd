b0b2d94932fe900ce0218aeda53a60996b630025
YongHyun-Yi <dldydgus1995@naver.com>
2021-02-09 03:27:41 +0900

    2021. 02. 09
    ai가 목표좌표 기반으로 이동하도록 수정, 시야를 회전시키려고 했지만 문제가 있음...터널링 비슷한 현상이 목격

--- enemy/enemy.gd
+++ enemy/enemy.gd
@@ -7,15 +7,20 @@ var target : Object = null
 
 var in_chase_area = false
 var chase_target : Object
+var last_sight_pos = Vector3()
 
-var in_detect_area = false
-var detect_target : Object
+var in_sight_area = false
+var in_sight_target : Object
 
 var dir
 var dis
 
 var chase_path : PoolVector3Array
 var chase_path_index = 0
+
+var waypoint : Vector3
+var waypoint_path : PoolVector3Array
+var waypoint_path_index = 0
 
 export (NodePath) var patrol_path_node
 var patrol_path = []
@@ -43,37 +48,24 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	$Label.text = "current state is : " + str(current_state) + "\nleft chase time is : " + str($chase_limit_timer.time_left)
+	$Label.text = "current state is : " + str(current_state) + "\nleft chase time is : " + str($chase_limit_timer.time_left) + "\ntarget is : " + str(target) + "\ndirection is : " + str(dir)
+	
 	var nav = get_parent().nav
 	
 	match current_state:
 		states.idle:
-			
-			if in_detect_area:
-				
-				dir = (detect_target.global_transform.origin - global_transform.origin).normalized()
-				dis = detect_target.global_transform.origin.distance_to(global_transform.origin)
-				
-				if $check_wall_down_side.cast_to == Vector3.ZERO and $check_wall_up_side.cast_to == Vector3.ZERO:
-					$check_wall_down_side.cast_to = dir * dis
-					$check_wall_up_side.cast_to = dir * dis
-				
-				else:
-					$check_wall_down_side.cast_to = dir * dis
-					$check_wall_up_side.cast_to = dir * dis
-					
-					if !$check_wall_down_side.is_colliding() or !$check_wall_up_side.is_colliding():
-						current_state = states.chase
-						chase_target = detect_target
-						if patrol_path.size() > 0: # 패트롤 경로가 존재할경우 스위칭
-							patrol_interrupted = true
-						$Sprite3D.modulate = Color("ff0000")
-						return
-				
-			else:
-				if $check_wall_down_side.cast_to != Vector3.ZERO and $check_wall_up_side.cast_to != Vector3.ZERO:
-					$check_wall_down_side.cast_to = Vector3.ZERO
-					$check_wall_up_side.cast_to = Vector3.ZERO
+	
+			if target == null:
+				if in_sight_object_raycast_check():
+					current_state = states.chase
+					nav_path_update()
+					if patrol_path.size() > 0: # 패트롤 경로가 존재할경우 스위칭
+						patrol_interrupted = true
+					$head/Sprite3D.modulate = Color("ff0000")
+					$bgm.stop()
+					$bgm.stream = load("res://sfx/bgm1.wav")
+					$bgm.play()
+					return
 			
 			if patrol_interrupted: # 패트롤이 도중에 방해받았을경우
 				if close_point == Vector3(): # close point가 없으면 설정한다
@@ -98,46 +90,93 @@ func _physics_process(delta):
 					print("is over")
 					
 				else: # 가장 가까운 패트롤 point까지 경로를 끝마치지 않은경우, 진행한다
-					dir = (patrol_restore_path[patrol_restore_path_index] - global_transform.origin)
+					dis = (patrol_restore_path[patrol_restore_path_index].distance_to(global_transform.origin))
+					dis = (patrol_restore_path[patrol_restore_path_index] - global_transform.origin).normalized()
 					
-					if dir.length() < 1:
+					if dis < 1:
 						patrol_restore_path_index += 1
 					else:
-						velocity = dir.normalized() * speed
+						velocity = dir * speed
 				
 			else: # 방해받지 않은경우, 평소대로 진행한다
 				if patrol_path.size() > 0: # 패트롤 경로가 존재할경우 진행
 					if patrol_path_index >= patrol_path.size():
 						patrol_path_index = 0
 						
-					dir = (patrol_path[patrol_path_index] - global_transform.origin)
+					dis = (patrol_path[patrol_path_index].distance_to(global_transform.origin))
+					dir = (patrol_path[patrol_path_index] - global_transform.origin).normalized()
 					
-					if dir.length() < 1:
+					if dis < 1:
 						patrol_path_index += 1
 					else:
-						velocity = dir.normalized() * speed
+						velocity = dir * speed
 			
 			pass
 		
 		states.suspect:
-			pass
+			
+			if in_sight_object_raycast_check():
+				current_state = states.chase
+				$chase_limit_timer.stop()
+				$head/Sprite3D.modulate = Color("ff0000")
+				$bgm.stop()
+				$bgm.stream = load("res://sfx/bgm1.wav")
+				$bgm.play()
+				return
+					
+			if waypoint != Vector3(): # 소리를 들었을경우, idle에서 넘어온거면 waypoint가 있지만 chase에서 넘어온것이면 한번 초기화를 하기떄문에 체크해줘야함
+				if waypoint_path.empty():
+					waypoint_path_setting(waypoint)
+			
+				if waypoint_path_index < waypoint_path.size():
+					
+					dis = (waypoint_path[waypoint_path_index].distance_to(global_transform.origin))
+					dir = (waypoint_path[waypoint_path_index] - global_transform.origin).normalized()
+					
+					if dis < 1:
+						waypoint_path_index += 1
+					else:
+						velocity = dir * speed
+				
+				else: # waypoint path를 끝마쳤을경우, 청각적 인지 목표위치까지 왔는데도 target을 발견하지 못했을 확률이 높음, 이 시점에서부터 타이머를 시작, target을 발견하거나 새 청각적 인지 목표가 나타나면 타이머를 중지
+					waypoint = Vector3()
+					waypoint_path.resize(0)
+					waypoint_path_index = 0
+					velocity = Vector3.ZERO
+					$chase_limit_timer.start()
+				pass
 		
 		states.chase:
 			
 			#dir = (chase_target.global_transform.origin - global_transform.origin).normalized()
 			#dis = chase_target.global_transform.origin.distance_to(global_transform.origin)
+			in_sight_object_raycast_check()
+			#nav_path_update()
+			if waypoint_path.empty():
+				waypoint_path_setting(waypoint)
 			
-			if chase_path.empty():
-				chase_path_setting(chase_target.global_transform.origin)
-			
-			if chase_path_index < chase_path.size():
+			if waypoint_path_index < waypoint_path.size():
 				
-				dir = (chase_path[chase_path_index] - global_transform.origin)
+				dis = (waypoint_path[waypoint_path_index].distance_to(global_transform.origin))
+				dir = (waypoint_path[waypoint_path_index] - global_transform.origin).normalized()
 				
-				if dir.length() < 1:
-					chase_path_index += 1
+				if dis < 1:
+					waypoint_path_index += 1
 				else:
-					velocity = dir.normalized() * speed
+					velocity = dir * speed
+			
+			else: # waypoint path를 끝마쳤을경우, target이 있으면 .25초마다 갱신됨으로 이 부분은 target을 놓친상태인 경우일 확률이 높다, 터널링으로 보이는 현상? path가 업데이트되어도 이전 paht를 그대로 따라감
+				current_state = states.suspect
+				target = null
+				waypoint = Vector3()
+				waypoint_path.resize(0)
+				waypoint_path_index = 0
+				velocity = Vector3.ZERO
+				$head/Sprite3D.modulate = Color("ffef00")
+				$chase_limit_timer.start()
+				$bgm.stop()
+				$bgm.stream = load("res://sfx/mbient_danger.wav")
+				$bgm.play()
 			
 			pass
 	
@@ -145,66 +184,147 @@ func _physics_process(delta):
 		velocity.y = 0
 	else:
 		velocity.y = -gravity
-		
+	
+	if target:
+		var a = (target.global_transform.origin - global_transform.origin).normalized()
+		a = Vector2(a.z, a.x)
+		a = rad2deg(a.angle())
+		$head.rotation_degrees.y = lerp($head.rotation_degrees.y, a, .1)
+	elif waypoint != Vector3():
+		var a = (waypoint - global_transform.origin).normalized()
+		a = Vector2(a.z, a.x)
+		a = rad2deg(a.angle())
+		$head.rotation_degrees.y = lerp($head.rotation_degrees.y, a, .1)
+	else:
+		var a = dir
+		a = Vector2(a.z, a.x)
+		a = rad2deg(a.angle())
+		$head.rotation_degrees.y = lerp($head.rotation_degrees.y, a, .1)
+	
 	move_and_slide(velocity, Vector3.UP)
 	
+	
+	var line3d = $line3d
+	var point_set
+	
+	line3d.global_transform.origin = Vector3.ZERO
+	line3d.clear()
+	line3d.begin(1, null) #1 = is an enum for draw line, null is for text
+	
+	if current_state == states.idle:
+		if patrol_interrupted:
+			point_set = patrol_restore_path
+		else:
+			point_set = patrol_path
+		line3d.set_color(Color("00ff16"))
+	else:
+		point_set = waypoint_path
+		line3d.set_color(Color("ff0000"))
+	
+	for i in range(point_set.size()):
+		if i + 1 < point_set.size():
+			var A = point_set[i]
+			var B = point_set[i + 1]
+			line3d.add_vertex(A)
+			line3d.add_vertex(B)
+	line3d.end()
+	
 	pass
-
 
 func nav_path_update():
 	
 	match current_state:
 		
 		states.idle:
-			velocity = Vector3.ZERO
+			pass
 		
 		states.suspect:
+			
 			pass
 		
 		states.chase:
-			chase_path_setting(chase_target.global_transform.origin)
+			#chase_path_setting(chase_target.global_transform.origin)
+			if target: # 퍼포먼스를 위해서 여기서 .25초마다 갱신, 타겟이 없으면 갱신을 멈춘다 즉 마지막으로 목격한 위치까지만
+				waypoint = target.global_transform.origin
+				waypoint_path_setting(waypoint)
 
 
-func chase_path_setting(target_pos):
-	
+func waypoint_path_setting(target_pos):
 	var nav = get_parent().nav
 	
-	chase_path = nav.get_simple_path(global_transform.origin, target_pos, true)
-	chase_path_index = 0
+	waypoint_path = nav.get_simple_path(global_transform.origin, target_pos, true)
+	waypoint_path_index = 0
 
-
-func _on_detect_body_entered(body):
+func _on_sight_area_body_entered(body):
 	print("something detected!")
-	if detect_target == null:
-		detect_target = body
+	if in_sight_target == null:
+		in_sight_target = body
 	
-	in_detect_area = true
+	in_sight_area = true
 	
 	pass # Replace with function body.
 
-func _on_detect_body_exited(body):
-	if detect_target != null:
-		detect_target = null
+func _on_sight_area_body_exited(body):
+	print("something lost....")
+	if in_sight_target != null:
+		in_sight_target = null
 	
-	in_detect_area = false
-	pass # Replace with function body.
-
-func _on_chase_area_body_entered(body):
-	#if !$chase_limit_timer.is_stopped():
+	in_sight_area = false
+	
 	if current_state == states.chase:
-		$chase_limit_timer.stop()
-		print("chase limit is canceled")
-		
-		in_chase_area = true
+		if body == target:
+			target = null
 	pass # Replace with function body.
 
+func in_sight_object_raycast_check():
+	
+	var sight_area = $head/sight_area
+	var check_wall = $check_wall
+	
+	if sight_area.get_overlapping_bodies().size() > 0:
+		for i in sight_area.get_overlapping_bodies():
+			dir = (i.global_transform.origin - global_transform.origin).normalized()
+			dis = i.global_transform.origin.distance_to(global_transform.origin)
+			check_wall.cast_to = dir * dis
+			check_wall.force_raycast_update()
+			
+			
+			if check_wall.is_colliding():
+				if target:
+					target = null
+			else:
+				target = i
+				return true
+	else:
+		if check_wall.cast_to != Vector3.ZERO:
+			check_wall.cast_to = Vector3.ZERO
+	return false
 
-func _on_chase_area_body_exited(body):
-	if current_state == states.chase:
-		$chase_limit_timer.start()
-		print("chase limit is start")
+
+func _on_auditory_area_area_entered(area):
+	print("sound area enterd")
+	match current_state:
+		states.idle:
+			waypoint = area.global_transform.origin
+			waypoint_path_setting(waypoint)
+			current_state = states.suspect
+			$head/Sprite3D.modulate = Color("ffef00")
+			$bgm.stop()
+			$bgm.stream = load("res://sfx/mbient_danger.wav")
+			$bgm.play()
 		
-		in_chase_area = false
+		states.suspect:
+			waypoint = area.global_transform.origin
+			waypoint_path_setting(waypoint)
+			if !$chase_limit_timer.is_stopped():
+				$chase_limit_timer.stop()
+		
+		states.chase:
+			if !target:
+				waypoint = area.global_transform.origin
+				waypoint_path_setting(waypoint)
+	
+	area.queue_free()
 	pass # Replace with function body.
 
 
@@ -213,6 +333,9 @@ func chase_limit_timer_timeout():
 	chase_target = null
 	chase_path.resize(0)
 	chase_path_index = 0
-	$Sprite3D.modulate = Color("ffffff")
+	$head/Sprite3D.modulate = Color("ffffff")
+	$bgm.stop()
+	$bgm.stream = load("res://sfx/271945__rodincoil__stingers-001.wav")
+	$bgm.play()
 	print("reset")
 	pass # Replace with function body.
--- player/player2.gd
+++ player/player2.gd
@@ -9,6 +9,16 @@ export var air_gravity = 7.0
 export var floor_gravity = 1.0
 export var floor_speed = 1.0
 
+onready var raycast_pos = $raycast_pos
+var raycast_pos_stand_height = 3.082
+var raycast_pos_crouch_height = 0.993
+
+onready var player_cam = $Camera
+var player_cam_stand_height = 3.117
+var player_cam_crouch_height = 1.757
+
+onready var sprint_sound_area = load("res://player_sound_area/player_sprint_sound_area.tscn")
+
 #export var mouse_sensitivity = 0.1
 
 var z_input = 0
@@ -19,11 +29,14 @@ export var walk_speed = 1.2
 export var run_speed = 2.8
 
 export var run_mode = false
+export var crouch_mode = false
 
 export var active = false
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	raycast_pos.transform.origin.y = raycast_pos_stand_height
+	player_cam.transform.origin.y = player_cam_stand_height
 
 func _physics_process(delta):
 	
@@ -35,6 +48,37 @@ func _physics_process(delta):
 			movement_speed = run_speed
 		else:
 			movement_speed = walk_speed
+		
+		if x_input != 0 or z_input != 0: # 이동중일때
+			if run_mode:
+				
+				if !$walking_sound_timer.is_stopped():
+					$walking_sound_timer.stop()
+					$walking_sound_player.stream = load("res://sfx/player_walking_01.wav")
+				
+				if $sprint_sound_timer.is_stopped():
+					$sprint_sound_timer.start()
+					$sprint_sound_player.play()
+					create_sprint_area()
+			
+			else: #걷는중일때
+				
+				if !$sprint_sound_timer.is_stopped():
+					$sprint_sound_timer.stop()
+					$sprint_sound_player.stream = load("res://sfx/player_sprint_01.wav")
+				
+				if $walking_sound_timer.is_stopped():
+					$walking_sound_timer.start()
+					$walking_sound_player.play()
+		
+		else: # 이동을 멈췄을떄
+			if !$sprint_sound_timer.is_stopped():
+				$sprint_sound_timer.stop()
+				$sprint_sound_player.stream = load("res://sfx/player_sprint_01.wav")
+				
+			if !$walking_sound_timer.is_stopped():
+				$walking_sound_timer.stop()
+				$walking_sound_player.stream = load("res://sfx/player_walking_01.wav")
 		
 		if is_on_floor():
 			gravity_vector = -get_floor_normal() * floor_gravity
@@ -77,6 +121,17 @@ func _unhandled_input(event):
 			run_mode = true
 		else:
 			run_mode = false
+		
+		if Input.is_action_just_pressed("crouch"):
+			
+			crouch_mode = !crouch_mode
+			
+			if crouch_mode:
+				raycast_pos.transform.origin.y = raycast_pos_crouch_height
+				player_cam.transform.origin.y = player_cam_crouch_height
+			else:
+				raycast_pos.transform.origin.y = raycast_pos_stand_height
+				player_cam.transform.origin.y = player_cam_stand_height
 
 func player_active():
 	active = true
@@ -91,3 +146,26 @@ func player_deactive():
 	run_mode = false
 	x_input = 0
 	z_input = 0
+
+func _on_walking_sound_timer_timeout():
+	if $walking_sound_player.stream == load("res://sfx/player_walking_01.wav"):
+		$walking_sound_player.stream = load("res://sfx/player_walking_02.wav")
+	else:
+		$walking_sound_player.stream = load("res://sfx/player_walking_01.wav")
+	$walking_sound_player.play()
+	pass # Replace with function body.
+
+func _on_sprint_sound_timer_timeout():
+	if $sprint_sound_player.stream == load("res://sfx/player_sprint_01.wav"):
+		$sprint_sound_player.stream = load("res://sfx/player_sprint_02.wav")
+	else:
+		$sprint_sound_player.stream = load("res://sfx/player_sprint_01.wav")
+	$sprint_sound_player.play()
+	create_sprint_area()
+	pass # Replace with function body.
+
+func create_sprint_area():
+	var sound_area = sprint_sound_area.instance()
+	get_parent().add_child(sound_area)
+	sound_area.global_transform.origin = global_transform.origin
+
